6ab0fd685300d9b5033e160af38a3981ddbd57eb
khalid jamal <kh.jamal95@gmail.com>
2018-08-07 01:09:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  let index = 0
-  let myEvent = document.body
-  
-myEvent.addEventListener('keydown', function onKeyDownHandler(e) {
-  
-  let key = parseInt(e.which || e.detail)
+  let index = 0;
 
-  if (key === code[0]) {
-    index++
-  }
-  if (index === code.length) {
-    allert("Hurray!")
-  } else {
-    index = 0
-  }
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
 
-})
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
