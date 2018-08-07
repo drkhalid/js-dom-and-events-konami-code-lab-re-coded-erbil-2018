557cd089026d8205da658d53e473c5100dec6184
khalid jamal <kh.jamal95@gmail.com>
2018-08-07 01:09:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
   let index = 0
-document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+  let myEvent = document.body
+  
+myEvent.addEventListener('keydown', function onKeyDownHandler(e) {
   
   let key = parseInt(e.which || e.detail)
 
