4cab95b010cf259d698e010ddda1ab1d1ba72f9d
khalid jamal <kh.jamal95@gmail.com>
2018-08-07 01:05:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,17 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-  let index = 0
-
-  let key = parseInt(e.which || e.detail)
-
-  if (key === code[0]) {
-    index++
-  }
-  if (index === code.length) {
-    allert("Hurray!")
-  } else {
-    index = 0
+  = 0
   }
 
 
